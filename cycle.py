4a2aeacccff805c511780f1ff9828616f43362fa
Jean Torres <jctorres4193@gmail.com>
2023-12-08 11:08:03 -0500

    Started python file

---
+++ cycle.py
@@ -0,0 +1,51 @@
+import requests 
+from bs4 import BeautifulSoup
+import time
+
+#while(True):
+print("works")
+
+response = requests.get('https://finance.yahoo.com/most-active')
+
+if response.status_code == 200:
+    print("Success/OK\nCode: " + str(response))
+elif response.status_code == 301:
+    print("Permanent Redirect\nCode: " + str(response))
+    #break
+elif response.status_code == 302:
+    print("Temporary Redirect\nCode: " + str(response))
+    #break
+elif response.status_code == 304:
+    print("Not Modified\nCode: " + str(response))
+    #break
+elif response.status_code == 401:
+    print("Unauthorized Access Error\nCode: " + str(response))
+    #break
+elif response.status_code == 403:
+    print("Forbidden\nCode: " + str(response))
+    #break
+elif response.status_code == 404:
+    print("Not Found\nCode: " + str(response))
+    #break
+elif response.status_code == 405:
+    print("Method Not Allowed\nCode: " + str(response))
+    #break
+elif response.status_code == 501:
+    print("Not Implemented\nCode: " + str(response))
+    #break
+elif response.status_code == 502:
+    print("Bad Gateway\nCode: " + str(response))
+    #break
+elif response.status_coe == 503:
+    print("Service Unavailable\nCode: " + str(response))
+    #break
+elif response.status_core == 504:
+    print("Gateway Timeout\nCode: " + str(response))
+    #break
+
+html_page = BeautifulSoup(response.text, "html.parser")
+
+
+time.sleep(10)
+
+print(response.text)
